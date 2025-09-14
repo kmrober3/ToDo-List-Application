e91c66bb5c76c22f8f124c1537283ba5f214cd62
kmrober3 <kmroberts@unc.edu>
2025-09-14 18:10:52 -0400

    Can add tasks with inline behavior

--- ToDoList.js
+++ ToDoList.js
@@ -1,3 +1,27 @@
-class ToDoList {
-    //ToDo
-}
+class ToDoList {  
+
+    constructor() {
+        this.currentTasks = [];  
+    }
+    
+    addItem(tasks) {
+        this.currentTasks.push(tasks); 
+        document.getElementById("tasks").textContent = this.currentTasks; 
+    } 
+
+    removeItem(tasks) {  
+        let removeTasksIndex = this.currentTasks.indexOf(tasks); 
+        this.currentTasks.splice(removeTasksIndex, 1);   
+        document.getElementById("tasks").textContent = "";
+        document.getElementById("tasks").textContent = this.currentTasks;
+    } 
+
+} 
+
+let instance = new ToDoList();
+
+document.getElementById("press").addEventListener("click", (event) => {  
+    //console.log("Yes");
+    let input = document.getElementById("input").value;
+    instance.addItem(input); 
+})
