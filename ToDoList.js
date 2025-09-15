f71332d7f29fedcf431b3247799f83a4ba2ce2e7
kmrober3 <kmroberts@unc.edu>
2025-09-15 18:52:07 -0400

    Added skeleton code for storage persistence

--- ToDoList.js
+++ ToDoList.js
@@ -1,31 +1,43 @@
 class ToDoList {  
 
     constructor() {
-        this.currentTasks = [];  
-    }
+        this.currentTasks = [];    
+        this.store = localStorage; 
+        this.count = 0;
+    } 
     
-    addItem(tasks) { 
+    addItem(tasks) {  
+        if (this.count == 0) {
+            this.store.clear();
+            this.count++;
+        }
         let taskDiv = document.getElementById("tasks");  
         this.currentTasks.push(tasks);    
         taskDiv.innerHTML = "";
-        this.currentTasks.forEach(t => { 
+        this.currentTasks.forEach(t => {  
             let li = document.createElement("li"); 
             let input = document.createElement("input");   
             input.type = "checkbox";    
             input.id = t; 
             li.textContent = t + " "; // add a space before button  
             li.appendChild(input); 
-            taskDiv.append(li);      
+            taskDiv.append(li);       
+            this.store.setItem(t, "not done");
         }); 
 
         document.getElementById("clear").addEventListener("click", (event) => { 
-            this.removeItem(taskDiv);
-        })
+            this.removeItem(taskDiv);  
+            console.log(this.store);
+        }) 
+        console.log(this.store);
     } 
 
     removeItem(taskDiv) { 
         this.currentTasks = this.currentTasks.filter(t => { 
-            let cb = document.getElementById(t);
+            let cb = document.getElementById(t);  
+            if (cb.checked) {
+                this.store.setItem(t, "done");
+            }
             return !cb.checked; 
         }) 
         
@@ -41,14 +53,24 @@ class ToDoList {
             li.appendChild(input);
             taskDiv.append(li);
         })
+    } 
+
+    reloadItems() {  
+        //ToDo
+
+
     }
 
 } 
 
 let instance = new ToDoList();
 
-document.getElementById("press").addEventListener("click", (event) => {  
+document.getElementById("press").addEventListener("click", (event) => {   
     console.log("Yes");
     let input = document.getElementById("input").value;
     instance.addItem(input); 
-}); 
+});  
+
+document.getElementById("listItems").addEventListener("DOMContentLoaded", (event) => {  
+    //ToDo
+})
